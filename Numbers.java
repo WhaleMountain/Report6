3c94d9f72c79d23434947dd53692438e5f5fe80e
Ken Miyahira <e175733@ie.u-ryukyu.ac.jp>
2017-12-31 16:06:33 +0900

    判定の関数化

--- Numbers.java
+++ Numbers.java
@@ -1,51 +1,68 @@
 import java.util.Scanner;
 public class Numbers{
     public static void main(String[] args){
+        int cnt = 1;
+        boolean result = false;
+        Scanner sc = new Scanner(System.in);
+        String question = setNumber();//問題の値の準備
+
+        //ユーザー入力と判定を繰返す
+        while(!result && cnt<=10){
+            System.out.print(cnt+"回目の入力:");
+            String user = sc.nextLine();//ユーザーの入力
+            result = judge(question,user);
+            cnt+=1;
+        }
+        //10回以内にクリアしないと負けになる
+        if(cnt>=10){
+            System.out.println("Your Lose...");
+        }else{
+            System.out.println("Your Win!!");
+        }
+    }
+
+    /*
+    * 問題の値とユーザー入力の値を判定する
+    */
+    public static boolean judge(String question, String user){
         int true_cnt=0;//入力された値と問題の値、位置が一緒の数。これが全て一緒なら勝利
         int false_cnt=0;//入力された値と問題の値が違う数
         int same_cnt=0;//入力された値と問題の値が一緒の数
-        int cnt = 1;
-        Scanner sc = new Scanner(System.in);
-        String question = setNumber();//問題の値の準備
+
         char[] question_num = question.toCharArray();//判定をしやすいよう一文字づつに区切る
-        
-        while(true){
-            System.out.print(cnt+"回目の入力:");
-            String user = sc.nextLine();//ユーザーの入力
-            char[] user_num = user.toCharArray();//判定しやすいよう一文字づつに区切る
+        char[] user_num = user.toCharArray();//判定しやすいよう一文字づつに区切る
 
-            //第一判定 True,Falseの数を求める
-            for(int i=0;i<4;i++){
-                if(question_num[i]==user_num[i]){
-                    true_cnt+=1;//入力された値、位置が一緒の数
-                }else{
-                    false_cnt+=1;//入力された値が間違っている数
-                }
+        //第一判定 True,Falseの数を求める
+        for(int i=0;i<4;i++){
+            if(question_num[i]==user_num[i]){
+                true_cnt+=1;//入力された値、位置が一緒の数
+            }else{
+                false_cnt+=1;//入力された値が間違っている数
             }
-
-            //第二判定 Sameの数を求める
-            for(int i=0;i<4;i++){
-                for(int j=0;j<4;j++){
-                    if(question_num[i]==user_num[j]){
-                        same_cnt+=1;//位置に関係なく同じ値の数
-                        if(question_num[j]==user_num[j]){
-                            same_cnt-=1;//Trueのカウント文も入るためその文引く
-                        }
-                        break;
-                    }
-                }
-            }
-            System.out.println("TRUE:"+true_cnt+" SAME:"+same_cnt+" FALSE:"+(false_cnt-same_cnt));//第一判定のfalseではsameの値もカウントしているため引き算をする
-            if(true_cnt==4){//全ての値と位置が正解したら勝利になる
-                System.out.println("Your Win!!");
-                break;
-            }
-            true_cnt=0;
-            false_cnt=0;
-            same_cnt=0;
-            cnt+=1;
-            //System.out.println(question);  デバック用(答えを表示する)
         }
+
+        //第二判定 Sameの数を求める
+        for(int i=0;i<4;i++){
+            for(int j=0;j<4;j++){
+                if(question_num[i]==user_num[j]){
+                    same_cnt+=1;//位置に関係なく同じ値の数
+                    if(question_num[j]==user_num[j]){
+                        same_cnt-=1;//Trueのカウント文も入るためその文引く
+                    }
+                    break;
+                }
+            }
+        }
+
+        System.out.println("TRUE:"+true_cnt+" SAME:"+same_cnt+" FALSE:"+(false_cnt-same_cnt));//第一判定のfalseではsameの値もカウントしているため引き算をする
+        if(true_cnt==4){//全ての値と位置が正解したら勝利になる
+            return true;
+        }
+        true_cnt=0;
+        false_cnt=0;
+        same_cnt=0;
+        //System.out.println(question);  デバック用(答えを表示する)
+        return false;
     }
 
     /*
@@ -87,5 +104,4 @@ public class Numbers{
         int number = (int)(Math.random()*9)+1;
         return number;
     }
-
 }
