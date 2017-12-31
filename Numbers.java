dafe808b605fee521dac9b7687cb0fcd4a365d90
Ken Miyahira <e175733@ie.u-ryukyu.ac.jp>
2017-12-31 12:02:33 +0900

    question numberの値の代入 3 --2の場合にはどのみち重複したため変更

--- Numbers.java
+++ Numbers.java
@@ -2,15 +2,29 @@ public class Numbers{
     public static void main(String[] args){
         int[] num = new int[4];//問題の値を代入する変数
         int[] tmp = new int[4];//同じ数があるかの判定に使う
+
         for(int i=0;i<4;i++){
-            num[i] = (int)(Math.random()*9)+1;//1~9の間でランダムに代入する
+            num[i] = randNumber();//1~9の間でランダムに代入する
             tmp[i] = num[i];
             for(int j=1;j<=i;j++){
                 if(num[i]==tmp[j-1]){//問題の値に同じ値があるか判断する
-                    num[i] = (int)(Math.random()*9)+1;//同じ値がある場合もう一度ランダムに代入する
+                    if(num[i]>=7){//7~9の値が被っていたら引き算をする
+                        num[i] -= j;
+                    }else{        //それ以外が被っている場合には足し算をする
+                        num[i] += j;
+                    }
                     tmp[i] = num[i];
                 }
             }
         }
     }
+
+    /*
+    * ランダムに値を生成する
+    */
+    public static int randNumber(){
+        int number = (int)(Math.random()*9)+1;
+        return number;
+    }
+
 }
