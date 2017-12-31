afdf02e765a6dbcff7ea5160d71b19d2e61683a3
Ken Miyahira <e175733@ie.u-ryukyu.ac.jp>
2017-12-31 11:54:57 +0900

    question numberの値の代入 2 --同じ値を代入しないようにする

--- Numbers.java
+++ Numbers.java
@@ -1,8 +1,16 @@
 public class Numbers{
     public static void main(String[] args){
         int[] num = new int[4];//問題の値を代入する変数
+        int[] tmp = new int[4];//同じ数があるかの判定に使う
         for(int i=0;i<4;i++){
             num[i] = (int)(Math.random()*9)+1;//1~9の間でランダムに代入する
+            tmp[i] = num[i];
+            for(int j=1;j<=i;j++){
+                if(num[i]==tmp[j-1]){//問題の値に同じ値があるか判断する
+                    num[i] = (int)(Math.random()*9)+1;//同じ値がある場合もう一度ランダムに代入する
+                    tmp[i] = num[i];
+                }
+            }
         }
     }
 }
