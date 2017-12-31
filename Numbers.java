16e157cb379569f3f364d32d3577457a1f48d450
Ken Miyahira <e175733@ie.u-ryukyu.ac.jp>
2017-12-31 15:33:13 +0900

    判定 1

--- Numbers.java
+++ Numbers.java
@@ -11,6 +11,15 @@ public class Numbers{
         String user = sc.nextLine();//ユーザーの入力
         char[] user_num = user.toCharArray();//判定しやすいよう一文字づつに区切る
 
+        //第一判定
+        for(int i=0;i<4;i++){
+            if(what_num[i]==user_num[i]){
+                true_cnt+=1;//入力された値、位置が一緒の数
+            }else{
+                false_cnt+=1;//入力された値が間違っている数
+            }
+        }
+
     }
 
     /*
