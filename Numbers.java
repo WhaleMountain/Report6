769e2a83293860608f2f918d27986d621f692424
Ken Miyahira <e175733@ie.u-ryukyu.ac.jp>
2018-01-01 12:50:33 +0900

    例外処理 3 --桁だけでなく数字以外の入力にも対応

--- Numbers.java
+++ Numbers.java
@@ -18,7 +18,13 @@ public class Numbers{
         while(!result && cnt<=10){
             System.out.print(cnt+"回目の入力:");
             String user = sc.nextLine();//ユーザーの入力
-            if(user.length()>4 || user.length()<4){
+            try{//数字以外が入力されている場合の例外処理
+                int i = Integer.parseInt(user);
+            }catch(NumberFormatException e){
+                System.out.println("数字を入力してください");
+                continue;
+            }
+            if(user.length()>4 || user.length()<4){//4桁以内、4桁以上で入力されていないか
                 System.out.println("4桁で入力してください");
                 continue;
             }
