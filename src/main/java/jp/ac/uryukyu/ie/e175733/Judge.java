c468173bbe6b16cc7339810209ac6d6f059b33f5
Ken Miyahira <e175733@ie.u-ryukyu.ac.jp>
2018-01-02 23:46:52 +0900

    クラス分け＆gradle --仕様書をよく見てなかったため作り直し。

---
+++ src/main/java/jp/ac/uryukyu/ie/e175733/Judge.java
@@ -0,0 +1,7 @@
+package jp.ac.uryukyu.ie.e175733;
+
+public class Judge {
+    public boolean judge(String question, String user){
+        return false;
+    }
+}
---
+++ src/main/java/jp/ac/uryukyu/ie/e175733/QuestionNumber.java
@@ -0,0 +1,10 @@
+package jp.ac.uryukyu.ie.e175733;
+
+public class QuestionNumber {
+    public String setNumber(){
+        return "";
+    }
+    private int randNumber(){
+        return 0;
+    }
+}
